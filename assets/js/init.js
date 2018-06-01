3827ccd25c7501f984ebc9784bc8e054bdd2962f
goergena <goergena@users.noreply.github.com>
2018-06-01 11:54:54 -0400

    portfolio

---
+++ assets/js/init.js
@@ -0,0 +1,16 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true
+    });
+
+    var bingeVid = "https://drive.google.com/file/d/17Yf85oPx6DrD91wf_P2qaf2AV33olyS2/view";
+    
+        
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
